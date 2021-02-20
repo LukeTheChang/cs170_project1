2fb5c01160a9e44ad8dd04a72e90d993d9dd58b4
LC <luccas.chang@gmail.com>
2021-02-20 02:16:26 -0800

    notable changes in priority queue operator overloads; able to somewhat implement or get a-star searches working; changed default puzzle, the one i had originally did not match the one in the project manual and is much easier to solve

--- cs170_main.cpp
+++ cs170_main.cpp
@@ -15,7 +15,9 @@ class node {
     public:
         std::vector<int> state;
         int weight = 0; //accumulated cost g(n)
-        int heur = 0; //heuristic h(n)
+        int heur_placeholder = 0; //for ucs search
+        int misplaced_heur = 0; //heuristic h(n)
+        int manhattan_heur = 0; //heuristic h(n)
 
         //basically check if 'tile' is zero and the tile above is within boundary of the puzzle; if so, swap tiles with the tile above
         void move_up(int tile) {
@@ -29,7 +31,7 @@ class node {
         }
 
         void move_down( int tile) {
-            if( (state.at(tile) == 0) && (tile+3) < 8) {
+            if( (state.at(tile) == 0) && (tile+3) <= 8) {
                 state.at(tile) = state.at(tile+3);
                 state.at(tile+3) = 0;
                 ++weight;
@@ -81,6 +83,7 @@ class node {
             }
         }
 
+        //how far off is the overall puzzle from the ideal state
         void misplaced() {
             std::vector<int> solution = {1,2,3,4,5,6,7,8,0};
             int off_by = 0; //essentially g(n) - how for off we are from the ideal state; how many more moves are needed to reach solution state
@@ -90,25 +93,60 @@ class node {
                     ++off_by;
                 }
             }
-            heur = off_by;
+            misplaced_heur = off_by;
         } 
+
+        //find the difference between slides and then add the differences to main
+        void manhattan() {
+            std::vector<int> solution = {1,2,3,4,5,6,7,8,0};
+            int i, j;
+            int diff = 0;
+            int manhattan = 0;
+            for(i = 0; i < solution.size(); ++i) {
+                for(j = 0; j < state.size(); ++j) {
+                    if(solution.at(i) == state.at(j)) {
+                        diff = abs(i - j);
+                        manhattan+=diff;
+                    }
+                }
+            }
+            manhattan_heur = manhattan;
+        }
 };
 
-struct compare_weight {
+
+//necessary to assist in priority_queue's sorting
+//renamed as default because if using UCS search, heur is zero
+//default essentially serves as a placeholder
+/* struct default_cost {
     bool operator()(node n1, node n2) {
         return n1.weight < n2.weight;
     }
+}; */
+
+struct default_heur { //for ucs, given that in the project prompt h(n) is always 0 in ucs search
+    bool operator()(node n1, node n2) {
+        //in this order, the smaller of the two values is given priority or given the top
+        return n1.weight > n2.weight;
+    }
 };
 
-struct compare_heur {
+struct misplaced_astar {
     bool operator()(node n1, node n2) {
-        return n1.heur < n2.heur;
+        return (n1.weight + n1.misplaced_heur) > (n2.weight + n2.misplaced_heur);
+    }
+};
+
+struct manhattan_astar {
+    bool operator()(node n1, node n2) {
+        return (n1.weight + n1.manhattan_heur) > (n2.weight + n2.manhattan_heur);
     }
 };
 
 
 void search_alg(std::vector<int> prob, int queueing_function);
-std::priority_queue<node, std::vector<node>, compare_heur> add_children(std::priority_queue<node, std::vector<node>, compare_heur> root);
+template<typename T>
+std::priority_queue<node, std::vector<node>, T> add_children(std::priority_queue<node, std::vector<node>, T> root);
 
 //unnecessary
 /*class tree {
@@ -136,7 +174,7 @@ void search_algo(node prob, int queueing_function) {
     node curr;
     if(queueing_function == 1) { //UCS search
         //std::cout << "test" << std::endl;
-        std::priority_queue<node, std::vector<node>, compare_heur> ucs_tree;
+        std::priority_queue<node, std::vector<node>, default_heur> ucs_tree;
         ucs_tree.push(prob);
         
         //std::cout << "test" << std::endl;
@@ -150,19 +188,80 @@ void search_algo(node prob, int queueing_function) {
             node curr = ucs_tree.top();
             if (curr.state == sol_state) {
                 std::cout << "solution found at depth or weight: " << curr.weight << std::endl;
+                std::cout << "current queue size: " << ucs_tree.size() << std::endl;
                 ucs_tree.top().print_puzzle();
                 return;
             } else {
                 std::cout << "current puzzle is the below with weight: " << ucs_tree.top().weight << std::endl;
+                std::cout << "current queue size: " << ucs_tree.size() << std::endl;
                 ucs_tree.top().print_puzzle();
             }
             ucs_tree = add_children(ucs_tree);
+            //return;
+        } while(!ucs_tree.empty());
+    } else if(queueing_function == 2) { //misplaced
+        //std::cout << "test" << std::endl;
+        std::priority_queue<node, std::vector<node>, misplaced_astar> ucs_tree;
+        prob.misplaced();
+        ucs_tree.push(prob);
+        
+        //std::cout << "test" << std::endl;
+        do {
+            //std::cout << "test" << std::endl;
+            if(ucs_tree.empty()) {
+                std::cout << "no solution found" << std::endl;
+                break;
+            }
+            //std::cout << "test" << std::endl;
+            node curr = ucs_tree.top();
+            if (curr.state == sol_state) {
+                std::cout << "solution found at weight: " << curr.weight << " and heur of: " << ucs_tree.top().misplaced_heur << std::endl;
+                std::cout << "current queue size: " << ucs_tree.size() << std::endl;
+                ucs_tree.top().print_puzzle();
+                return;
+            } else {
+                std::cout << "current puzzle is the below with weight: " << ucs_tree.top().weight << " and heur of: " << ucs_tree.top().misplaced_heur << std::endl;
+                std::cout << "current queue size: " << ucs_tree.size() << std::endl;
+                ucs_tree.top().print_puzzle();
+            }
+            ucs_tree = add_children(ucs_tree);
+            //return;
+        } while(!ucs_tree.empty());
+    } else if(queueing_function == 3) { //manhattan
+        //std::cout << "test" << std::endl;
+        std::priority_queue<node, std::vector<node>, manhattan_astar> ucs_tree;
+        prob.manhattan();
+        ucs_tree.push(prob);
+        
+        //std::cout << "test" << std::endl;
+        do {
+            //std::cout << "test" << std::endl;
+            if(ucs_tree.empty()) {
+                std::cout << "no solution found" << std::endl;
+                break;
+            }
+            //std::cout << "test" << std::endl;
+            node curr = ucs_tree.top();
+            if (curr.state == sol_state) {
+                std::cout << "solution found at weight: " << curr.weight << " and heur of: " << ucs_tree.top().misplaced_heur << std::endl;
+                std::cout << "current queue size: " << ucs_tree.size() << std::endl;
+                ucs_tree.top().print_puzzle();
+                return;
+            } else {
+                std::cout << "current puzzle is the below with weight: " << ucs_tree.top().weight << " and heur of: " << ucs_tree.top().misplaced_heur << std::endl;
+                std::cout << "current queue size: " << ucs_tree.size() << std::endl;
+                ucs_tree.top().print_puzzle();
+            }
+            ucs_tree = add_children(ucs_tree);
+            //return;
         } while(!ucs_tree.empty());
     }
 }
 
-std::priority_queue<node, std::vector<node>, compare_heur> add_children(std::priority_queue<node, std::vector<node>, compare_heur> root) {
-    std::priority_queue<node, std::vector<node>, compare_heur> new_tree = root;
+
+template<typename T>
+std::priority_queue<node, std::vector<node>, T> add_children(std::priority_queue<node, std::vector<node>, T> root) {
+    std::priority_queue<node, std::vector<node>, T> new_tree = root;
     node base = new_tree.top();
     int i = 0;
     new_tree.pop(); //remove root since it was scanned and deemed not to be the desired state
@@ -185,10 +284,38 @@ std::priority_queue<node, std::vector<node>, compare_heur> add_children(std::pri
     right.move_right(find_zero);
 
     //check if puzzle has been modified; if so, push up to queue or tree
-    if(up.state != base.state) {new_tree.push(up);}
-    if(down.state != base.state) {new_tree.push(down);}
-    if(left.state != base.state) {new_tree.push(left);}
-    if(right.state != base.state) {new_tree.push(right);} 
+    if(up.state != base.state) {
+        //std::cout <<"up" << std::endl; 
+        up.misplaced();
+        up.manhattan();
+        new_tree.push(up);
+    }
+    if(right.state != base.state) {
+        //std::cout <<"right" << std::endl; 
+        right.misplaced();
+        right.manhattan();
+        new_tree.push(right);
+    } 
+    if(down.state != base.state) {
+        //std::cout <<"down" << std::endl; 
+        down.misplaced();
+        down.manhattan();
+        new_tree.push(down);
+    }
+    if(left.state != base.state) {
+        //std::cout <<"left" << std::endl; 
+        left.misplaced();
+        left.manhattan();
+        new_tree.push(left);
+    }
+
+    
+    //test new node output
+    /* std::priority_queue<node, std::vector<node>, T> temp_tree = new_tree;
+    for(i=0; i < new_tree.size(); ++i) {
+        temp_tree.top().print_puzzle();
+        temp_tree.pop();
+    } */
 
     return new_tree;
 }
@@ -196,7 +323,7 @@ std::priority_queue<node, std::vector<node>, compare_heur> add_children(std::pri
 int main() {
     int default_puzzle = 1;
     int search_alg = 0;
-    std::vector<int> puzzle = {1, 2, 3, 4, 0, 5, 6, 7, 8}; //default puzzle
+    std::vector<int> puzzle = {1, 2, 3, 4, 0, 6, 7, 5, 8}; //default puzzle
     node basic;
     int x, y, z;
 
